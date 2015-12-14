0ce782cac52eb3c80cfa0afe90e84f8d4e9b0643
Nolan Neustaeter <nolan@flexdealer.com>
2015-12-13 19:24:32 -0800

    adding models

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,7 +6,12 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateBusinessesTable' => $baseDir . '/database/migrations/2015_12_14_031509_create_businesses_table.php',
+    'CreateCategoriesTable' => $baseDir . '/database/migrations/2015_12_14_032341_create_categories_table.php',
+    'CreatePagesTable' => $baseDir . '/database/migrations/2015_12_14_032020_create_pages_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+    'CreateProductsTable' => $baseDir . '/database/migrations/2015_12_14_032143_create_products_table.php',
+    'CreateTagsTable' => $baseDir . '/database/migrations/2015_12_14_032334_create_tags_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
