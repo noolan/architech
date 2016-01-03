07713dd1230b095ff614d62c76188351aaa38bb9
Nolan Neustaeter <nolan@flexdealer.com>
2016-01-02 18:52:29 -0800

    DB Seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        $this->call(BusinessTableSeeder::class);
+        $this->call(BusinessesTableSeeder::class);
 
         Model::reguard();
     }
